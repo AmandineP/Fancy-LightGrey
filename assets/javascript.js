eb022a60679b02f864cf54a4ffa406f09cac3f1a
AmandineP <amandine.pean@free.fr>
2016-03-08 23:04:05 +0100

    Java menu + bouton retour

--- assets/javascript.js
+++ assets/javascript.js
@@ -8,3 +8,23 @@ $(function(){
       }
    });
  });
+
+
+
+$(document).ready( function () {
+    // Add return on top button
+    $('body').append('<div id="scrollToTop" title="Retour en haut">&nbsp;</div>');
+    
+    // On button click, let's scroll up to top
+    $('#scrollToTop').click( function() {
+        $('html,body').animate({scrollTop: 0}, 'slow');
+    });
+});
+
+$(window).scroll(function() {
+    // If on top fade the bouton out, else fade it in
+    if ( $(window).scrollTop() == 0 )
+        $('#scrollToTop').fadeOut();
+    else
+        $('#scrollToTop').fadeIn();
+});
