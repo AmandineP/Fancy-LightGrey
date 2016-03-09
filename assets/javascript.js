5dd4a00fb89ddd92eeb1002ffabbcaa457c865db
AmandineP <amandine.pean@free.fr>
2016-03-09 13:14:31 +0100

    Rectification java

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,27 +2,25 @@ var hauteur = 400;
 $(function(){
    $(window).scroll(function () {
       if ($(this).scrollTop() > hauteur) { 
-            $('.Sommaire').show();
+            $('.Sommaire').fadeIn();
       } else {
-            $('.Sommaire').hide();
+            $('.Sommaire').fadeOut();
       }
    });
  });
 
-
-
 $(document).ready( function () {
-    // Add return on top button
+    
     $('body').append('<div id="scrollToTop" title="Retour en haut">&nbsp;</div>');
     
-    // On button click, let's scroll up to top
+    
     $('#scrollToTop').click( function() {
         $('html,body').animate({scrollTop: 0}, 'slow');
     });
 });
 
 $(window).scroll(function() {
-    // If on top fade the bouton out, else fade it in
+    
     if ( $(window).scrollTop() == 0 )
         $('#scrollToTop').fadeOut();
     else
